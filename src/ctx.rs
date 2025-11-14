243e4f9e26f09a8ea83e5a7245146c78acc6841c
dani <sourdanidev@gmail.com>
2025-11-13 20:31:39 -0500

    feat: mod specific butler staging foler

--- src/ctx.rs
+++ src/ctx.rs
@@ -98,13 +98,13 @@ impl RunnerContext
     }
 
     /// Get staging location for butler.
-    /// {sourcemod_dir}{crate::STAGING_DIR}
+    /// {sourcemod_dir}{crate::staging_dir()}
     /// e.g; /home/kate/.var/app/com.valvesoftware.Steam/.local/share/Steam/
     /// steamapps/sourcemods/butler-staging      C:\Games\Steam\steamapps\
     /// sourcemods\butler-staging
     pub fn get_staging_location(&mut self) -> String
     {
-        helper::join_path(self.sourcemod_path.clone(), crate::STAGING_DIR.to_string())
+        helper::join_path(self.sourcemod_path.clone(), crate::staging_dir())
     }
 
     /// Get the latest item in `remote_version_list`
